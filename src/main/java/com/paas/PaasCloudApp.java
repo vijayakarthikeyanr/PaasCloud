97f97258f96f766410e5e501cd3faf7f967e1233
Karthikeyan <vijayakarthikeyan.r@gmail.com>
2023-08-07 20:44:23 +0530

    pass cloud app changes
    pass cloud app changes

--- src/main/java/com/paas/PaasCloudApp.java
+++ src/main/java/com/paas/PaasCloudApp.java
@@ -1,32 +1,34 @@
 package com.paas;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 import com.paas.app.config.CloudFoundryConfig;
 
+@ComponentScan(basePackages = { "com.paas.app" })
 @SpringBootApplication
-public class PaasCloudApp {
+public class PaasCloudApp implements CommandLineRunner {
 
 	@Autowired
 	CloudFoundryConfig cloudFoundryConfig;
 
+	private static Logger LOG = LoggerFactory.getLogger(PaasCloudApp.class);
+
 	public static void main(String[] args) {
+		LOG.info("STARTING : Spring boot application starting");
 		SpringApplication.run(PaasCloudApp.class, args);
+		LOG.info("STOPPED  : Spring boot application stopped");
 	}
 
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-			this.start();
-		};
-	}
-
-	public void start() {
+	@Override
+	public void run(String... args) throws Exception {
+		LOG.info("EXECUTING : command line runner");
 		cloudFoundryConfig.generatePassReportFromCloudFoundry();
 	}
+
 }
